03840624b41f00cba0519aef07a6f49dee32fcc8
Jonathan Cady <joncady@uw.edu>
2019-05-08 13:09:57 -0700

    Registered with admin

--- dbProject/dbApp/admin.py
+++ dbProject/dbApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Patient, Provider, Appointment
 
 # Register your models here.
+admin.site.register(Patient)
+admin.site.register(Provider)
+admin.site.register(Appointment)
+
