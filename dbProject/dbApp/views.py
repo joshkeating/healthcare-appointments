746f2ba713252b7ca60a6e68f5b812509053044b
joshkeating <josh.keating7@gmail.com>
2019-05-10 03:15:58 -0400

    register and login form skeletons

--- dbProject/dbApp/views.py
+++ dbProject/dbApp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def login(request):
+
+	if request.method == 'POST':
+
+        return
+    else:
+        return
+
+
+
+def register(request):
+
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+
+		if form.is_valid():
+            
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+
+            # auth and login user
+			user = authenticate(username=username, password=password)
+            login(request, user)
+			
+			# redirect here
+	else:
+		# return the form if no input
+		form = UserCreationForm()
+	
