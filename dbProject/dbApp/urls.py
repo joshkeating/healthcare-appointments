578c6954e13b74fb03164007ae8bce11d2b0b057
joshkeating <josh.keating7@gmail.com>
2019-05-13 13:56:58 -0400

    added appointment api url

--- dbProject/dbApp/urls.py
+++ dbProject/dbApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MedicationAPI, PrescriptionAPI, AdminAPI
+from .views import MedicationAPI, PrescriptionAPI, AdminAPI, AppointmentAPI
 
 
 urlpatterns = [
     path(r'medications', MedicationAPI.as_view()),
     path(r'prescriptions', PrescriptionAPI.as_view()),
+    path(r'appointment', AppointmentAPI.as_view()),
     path(r'admins', AdminAPI.as_view()),
 ]
