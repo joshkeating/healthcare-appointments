d644c44cee2481904ca03fe0104c949481058cc7
joshkeating <josh.keating7@gmail.com>
2019-05-31 14:53:16 -0400

    scraped meds

--- dbProject/scrape-med/scrape-drugs.py
+++ dbProject/scrape-med/scrape-drugs.py
@@ -10,6 +10,7 @@ import re
 
 
 def get_medication_names():
+    '''get top 40 drugs from drugs.com'''
 
     base_url = 'https://www.drugs.com/drug_information.html'
     target_page = requests.get(base_url)
@@ -25,27 +26,70 @@ def get_medication_names():
     return drug_list
 
 
-def get_info():
+def get_info(meds):
     '''get info for meds'''
 
-    # url = 'https://www.drugs.com/' + test + '.html'
+    desc_list = []
 
-    url = 'https://www.drugs.com/meloxicam.html'
+    for med in meds:
 
-    target_page = requests.get(url)
-    html = bs(target_page.text, "html.parser")
+        url = 'https://www.drugs.com/' + med + '.html'
 
-    first_five = html.find_all("p", limit=5)
-    target_desc = first_five[2:]
+        target_page = requests.get(url)
+        html = bs(target_page.text, "html.parser")
 
-    cur_desc = ""
+        first_five = html.find_all("p", limit=5)
+        target_desc = first_five[2:]
 
-    for elem in target_desc:
-        cur_desc += elem.text + " "
+        cur_desc = ""
 
-    print(cur_desc)
+        for elem in target_desc:
+            cur_desc += elem.text + " "
+
+        desc_list.append(cur_desc)
+
+    return desc_list
+
+
+def get_dosage(meds):
+    '''TODO'''
+
+    dosage_list = []
+
+    for med in meds:
+
+        url = 'https://www.drugs.com/dosage/'  + med + '.html'
+
+        target_page = requests.get(url)
+        html = bs(target_page.text, "html.parser")
+
+        first_two = html.find_all("p", limit=2)
+        target = first_two[1:]
+        cur_dosage = target[0].text
+
+        dosage_list.append(cur_dosage)
+
+    return dosage_list
+
+
+def main():
+
+    # scrape all needed info
+    med_list = get_medication_names()
+    desc_list = get_info(med_list)
+    dosage_list = get_dosage(med_list)
+
+    # create tuples from lists
+    rows = zip(med_list, desc_list, dosage_list)
+
+    # write to file
+    with open('./medications.csv', "w") as f:
+        writer = csv.writer(f)
+        for row in rows:
+            writer.writerow(row)
 
     pass
 
 
-get_info()
+if __name__ == "__main__":
+    main()
